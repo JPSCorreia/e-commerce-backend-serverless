dd705ab2a024dd0d4b94acc447f80461819f8213
João Correia <jpcorreia@mail.com>
2023-10-27 12:10:50 +0100

    vercel support

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ app.use(limiter);
 const apiRouter = require('./api/api');
 app.use('/api', apiRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 module.exports = app;
