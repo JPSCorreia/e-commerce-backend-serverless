c720c8d418f4cf44bb860b11a5d1aac50c2ed51a
João Correia <jpcorreia@mail.com>
2023-10-31 15:49:52 +0000

    1

--- src/api/products.js
+++ src/api/products.js
@@ -5,12 +5,12 @@ const checkJwt = require('../middleware/authorization');
 const allowCors = require('../allowCors');
 
 // GET request for entire products table
-productsRouter.get('/', productQueries.getAllProducts);
+productsRouter.get('/', allowCors(productQueries.getAllProducts));
 
-productsRouter.get('/teste', productQueries.teste);
+productsRouter.get('/teste', allowCors(productQueries.teste));
 
 // GET request for single product by id
-productsRouter.get('/:id', productQueries.getProductById);
+productsRouter.get('/:id', allowCors(productQueries.getProductById));
 
 // GET request for products page
 productsRouter.get('/page/:page', checkJwt, allowCors(productQueries.getProductPage));
@@ -19,33 +19,33 @@ productsRouter.get('/page/:page', checkJwt, allowCors(productQueries.getProductP
 productsRouter.get(
   '/discounted/:number',
   checkJwt,
-  productQueries.getMostDiscountedProducts
-);
+  allowCors(productQueries.getMostDiscountedProducts
+));
 
 // GET request for total number of products
 productsRouter.get(
   '/total/get_number',
   checkJwt,
-  productQueries.getNumberOfProducts
-);
+  allowCors(productQueries.getNumberOfProducts
+));
 
 // POST request for searching for products
 productsRouter.post(
   '/search_products/search',
   checkJwt,
-  productQueries.getSearchResults
-);
+  allowCors(productQueries.getSearchResults
+));
 
 // POST request for adding a new product
-productsRouter.post('/', checkJwt, productQueries.createProduct);
+productsRouter.post('/', checkJwt, allowCors(productQueries.createProduct));
 
 // UPDATE request for adding existing product stock
-productsRouter.put('/add_stock', checkJwt, productQueries.addStock);
+productsRouter.put('/add_stock', checkJwt, allowCors(productQueries.addStock));
 
 // UPDATE request for removing existing product stock
-productsRouter.put('/', checkJwt, productQueries.removeStock);
+productsRouter.put('/', checkJwt, allowCors(productQueries.removeStock));
 
 // DELETE request for deleting existing product
-productsRouter.delete('/:id', checkJwt, productQueries.deleteProduct);
+productsRouter.delete('/:id', checkJwt, allowCors(productQueries.deleteProduct));
 
 module.exports = productsRouter;
