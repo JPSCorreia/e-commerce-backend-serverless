a7c40ea6262d92dc33315e3881b6f3f4bbb79ec2
João Correia <jpcorreia@mail.com>
2023-10-31 15:17:24 +0000

    1

--- src/app.js
+++ src/app.js
@@ -44,8 +44,8 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/test-cors', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.get('/test-cors', allowCors, (req, res) => {
+  // res.header('Access-Control-Allow-Origin', '*');
   res.send('CORS test successful');
 });
 
